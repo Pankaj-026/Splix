ca1fe2261e34e0df1daa77ed4bf29700a175073d
Pankaj-026 <pankajgupta1063@gmail.com>
2025-03-02 00:21:16 +0530

    Enhance Login component with background image and sign-in form

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,26 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_small.jpg"
-        alt="bg-image"
-      />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_small.jpg"
+          alt="bg-image"
+        />
+      </div>
+      <form className="w-3/12 absolute bg-black p-12 my-36 mx-auto right-0 left-0">
+      <h2>Sign In</h2>
+        <input
+          type="email"
+          placeholder="Email Address"
+          className="p-2 m-2"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 m-2"
+        />
+        <button className="p-2 m-2 bg-red-600 text-white">Sign In</button>
+      </form>
     </div>
   );
 };
