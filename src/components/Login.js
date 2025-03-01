d22035d8bf35a7990a5a5220372d643a852090de
Pankaj-026 <pankajgupta1063@gmail.com>
2025-03-02 00:07:26 +0530

    Add routing and main components; remove unused assets

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <img
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/04ef06cc-5f81-4a8e-8db0-6430ba4af286/web/IN-en-20250224-TRIFECTA-perspective_3a9c67b5-1d1d-49be-8499-d179f6389935_small.jpg"
+        alt="bg-image"
+      />
+    </div>
+  );
+};
+
+export default Login;
